76be0096de56c543ae5e43ea45f96d7844d0791f
Alejandro Garcia <AlexDG997@gmail.com>
2024-04-10 10:05:44 -0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Google Drive</title>
+    <link rel="stylesheet" href="{{url('css/index.css')}}">
+    <link rel="shortcut icon" href="{{url('https://ssl.gstatic.com/images/branding/product/2x/hh_drive_36dp.png')}}" type="image/x-icon">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="container2">
+                <div id="logoBox">
+                    <img src="{{asset('img/Google-Drive-Emblem.png')}}" alt="Logo.png" id="logo">
+                </div>
+                <div id="descripcionBox">
+                    <p style="color:rgb(114, 114, 114)">Descripcion General</p>
+                </div>
+            </div>
+            <div id="logIn">
+                <a href="inicioSesion">Iniciar Sesion</a>
+            </div>
+        </div class="container">
+        <hr>
+    </header>
+        <section id="landing">
+            <img src="{{asset('img/landing.png')}}" alt="landing.png" style="margin-top: 1.5%">
+        </section>
+</body>
+</html>
